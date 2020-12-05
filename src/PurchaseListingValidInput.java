fa0dea4234611b8c33e8f0c01b22835b7e1c2976
Shihang Li <sli01@unomaha.edu>
2020-12-05 17:01:13 -0600

    Purchase Listing Valid Input Test Case (Corrected file name)

--- src/PurchaseListingValidInput.java
+++ src/PurchaseListingValidInput.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class PurchaseLisitingValidInput {
+public class PurchaseListingValidInput {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
