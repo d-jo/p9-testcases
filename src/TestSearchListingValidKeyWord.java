509cdb5514c743c55bd543172bae43a598d462d6
Shihang Li <sli01@unomaha.edu>
2020-12-05 19:29:54 -0600

    Search for Listings with a key word

---
+++ src/TestSearchListingValidKeyWord.java
@@ -0,0 +1,97 @@
+import java.util.regex.Pattern;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestSearchListingValidKeyWord {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  @Before
+  public void setUp() throws Exception {
+	ChromeOptions options = new ChromeOptions();
+	
+	String os = System.getProperty("os.name").toLowerCase();
+	System.err.println(os);
+	if (os.contains("windows")) {
+		System.setProperty("webdriver.chrome.driver", "lib/win/chromedriver.exe");
+	} else if (os.contains("linux")){
+		System.setProperty("webdriver.chrome.driver", "lib/linux/chromedriver");
+		options.setBinary(new File("/usr/bin/chromium-browser"));
+	} else {
+		System.setProperty("webdriver.chrome.driver", "lib/mac/chromedriver");
+	}
+	 
+    options.addArguments("start-maximized");
+	driver = new ChromeDriver(options);
+	
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+
+  @Test
+  public void testSearchListingValidKeyWord() throws Exception {
+    driver.get("http://localhost:7000/");
+    Thread.sleep(500);
+
+    driver.findElement(By.id("search")).click();
+    driver.findElement(By.id("search")).clear();
+    driver.findElement(By.id("search")).sendKeys("Algorithm");
+    Thread.sleep(500);
+
+    driver.findElement(By.xpath("//button[@onclick='search()']")).click();
+    Thread.sleep(500);
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
